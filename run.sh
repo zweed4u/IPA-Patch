8054665bd143f4af1031f9d33a1d6da98ace93b6
Zachary Weeden <zweeden@datto.com>
2018-05-03 22:13:05 -0400

    Hooking instagram and added handyipa changes from upstream

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #Get the IPA
-wget https://people.rit.edu/zdw7287/files/Snapchat_10.29.0.33.ipa
+wget https://people.rit.edu/zdw7287/files/Instagram_43.ipa
 
 # Make use of HandyIPAPatcher
-cp Snapchat_10.29.0.33.ipa HandyIPAPatcher/app.ipa
-cd HandyIPAPatcher && ./extract_thin.sh Snapchat && cp Snapchat_arm* ../hexbytescanner/
+cp Instagram_43.ipa HandyIPAPatcher/app.ipa
+cd HandyIPAPatcher && ./extract_thin.sh Instagram && cp Instagram_arm* ../hexbytescanner/
 
 #Copy json automation hooking over
 cp ../hexbytescanner-hooks/openssl_hook_v2.json ../hexbytescanner/openssl_hook_v2.json
 
 #Build hexbytescanner
-cd ../hexbytescanner && ./build.sh && ./hexbytescanner openssl_hook_v2.json Snapchat_arm64 && cp Snapchat_arm64_patched ../HandyIPAPatcher/Snapchat_arm64
+cd ../hexbytescanner && ./build.sh && ./hexbytescanner openssl_hook_v2.json Instagram_arm64 && cp Instagram_arm64_patched ../HandyIPAPatcher/Instagram_arm64
 
 #Repack
-cd ../HandyIPAPatcher && ./replace_bin.sh Snapchat fat trim
+cd ../HandyIPAPatcher && ./replace_bin.sh Instagram fat trim
 
 echo 'Patched app HandyIPAPatcher/app.ipa can be installed via Cydia Impactor or AppCake'
